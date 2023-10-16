2b7e08acfdf50bb8c86bd86642f6fb63d4f96da3
lqd <quangdule5@gmail.com>
2023-10-16 14:25:55 +0700

    update 16/10/2023

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,83 +1,92 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup, Validators, Form } from '@angular/forms';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  Form,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit  {
-  submitted= false;
+export class SignupComponent implements OnInit {
+  submitted = false;
   token: any;
 
-  constructor(private  http: HttpClient,private fbuilder:FormBuilder, private router: Router,private toastr: ToastrService){
-
-  } 
+  constructor(
+    private http: HttpClient,
+    private fbuilder: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
   ngOnInit(): void {
     if ((this.token = localStorage.getItem('token'))) {
       this.router.navigate(['/']);
     }
   }
-  UserArray : any[] = [];
-  name: string ="";
-  email: string ="";
-  password: string ="";
-  password_confirm: string ="";
-  currentUserID = "";
+  UserArray: any[] = [];
+  name: string = '';
+  email: string = '';
+  address: string = '';
+  phone: string = '';
+  password: string = '';
+  password_confirm: string = '';
+  currentUserID = '';
   // Alluser()
-  // { 
+  // {
   //   this.http.get("http://127.0.0.1:8000/api/user")
-  
+
   //   .subscribe((resultData: any)=>
   //   {
   //       this.UserArray = resultData;
 
   //   });
   // }
-  register()
-  {
-    this.submitted= true;
+  register() {
+    this.submitted = true;
 
     let bodyData = {
-      "name" : this.name,
-      "email" : this.email,
-      "password" : this.password,
-      "password_confirm" : this.password_confirm,
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.password_confirm,
     };
-    this.http.post("http://127.0.0.1:8000/api/user",bodyData).subscribe((resultData: any)=>
-    {
-        if(resultData["code"] === 401){
-          this.toastr.error(JSON.stringify(resultData['messeage']),'',{
-            timeOut:1000,
-            progressBar:true
-          })
+    this.http
+      .post('http://127.0.0.1:8000/api/user', bodyData)
+      .subscribe((resultData: any) => {
+        if (resultData['code'] === 401) {
+          this.toastr.error(JSON.stringify(resultData['messeage']), '', {
+            timeOut: 1000,
+            progressBar: true,
+          });
           this.password = '';
         }
-        if(resultData["code"] === 409){
-          this.toastr.error(JSON.stringify(resultData['messeage']),'',{
-            timeOut:1000,
-            progressBar:true
-          })
+        if (resultData['code'] === 409) {
+          this.toastr.error(JSON.stringify(resultData['messeage']), '', {
+            timeOut: 1000,
+            progressBar: true,
+          });
           this.password = '';
         }
-        if(resultData["code"] === 200){
-          this.toastr.success(JSON.stringify(resultData['messeage']),'',{
-            timeOut:1000,
-            progressBar:true
-          })
+        if (resultData['code'] === 200) {
+          this.toastr.success(JSON.stringify(resultData['messeage']), '', {
+            timeOut: 1000,
+            progressBar: true,
+          });
           this.name = '';
           this.email = '';
           this.password = '';
           this.router.navigate(['/signin']);
         }
-    });
+      });
   }
-  save()
-  { 
-        this.register();
+  save() {
+    this.register();
   }
 }
